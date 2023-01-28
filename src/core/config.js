b42e00d515d588721af79807822ed0c86f822dcd
amirvatani <amirhosseinvatani75@gmail.com>
2023-01-28 20:12:28 +0330

    [feature] firebase config handled (matched with our version)

--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,10 @@
-// Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBDsdN3XWM5D2-c_ti5j44GrhMnBHNmSF0',
-  authDomain: 'react-native-market-2861b.firebaseapp.com',
-  databaseURL: 'https://react-native-market-2861b.firebaseio.com',
-  projectId: 'react-native-market-2861b',
-  storageBucket: 'react-native-market-2861b.appspot.com',
-  messagingSenderId: '878215484396',
-  appId: '1:878215484396:web:c36074bccb0b5693b0a6c9',
-  measurementId: 'G-317PLP59P2',
+  apiKey: 'AIzaSyD2KmM1Rp_hf14LDLpBWkAlIn_VubGUsxo',
+  authDomain: 'hidemypic-2a0cb.firebaseapp.com',
+  databaseURL: 'https://hidemypic-2a0cb-default-rtdb.firebaseio.com',
+  projectId: 'hidemypic-2a0cb',
+  storageBucket: 'hidemypic-2a0cb.appspot.com',
+  messagingSenderId: '928829338133',
+  appId: '1:928829338133:web:48426a69445d4r38742941c',
+  measurementId: 'G-96HWSNRMWS',
 }
